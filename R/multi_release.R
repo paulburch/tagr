e6cb5f2239b27bd1b2f9c9508211a0048cb38550
Paul Burch <Paul.Burch@aad.gov.au>
2017-03-14 06:24:59 +1100

    new function 'multi_release' to calculate population size from multiple release cohorts

---
+++ R/multi_release.R
@@ -0,0 +1,11 @@
+## population size with bootstrapped uncertainty using multiple releases
+
+
+## this function now takes a vector of tags, catch and recaptures
+check_mrelease_inputs <- function(tags, catch, recaps){
+  ## define the check variable
+  check <- TRUE
+  ## add checks
+  ## return check
+  check
+}
