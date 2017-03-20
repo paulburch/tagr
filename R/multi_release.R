e69ea2129333a2797a6e432180963f6dda035599
Paul Burch <Paul.Burch@aad.gov.au>
2017-03-21 03:45:20 +1100

    estimate population size from multiple release cohorts

--- R/multi_release.R
+++ R/multi_release.R
@@ -1,52 +1,67 @@
 ## population size with bootstrapped uncertainty using multiple releases
-##
-## TODO
-## function to check the pars and expand any that are the wrong length
-## The current code allows the available tags to be negative
-## Ricker Type 2 fishery is untested
 
+#' Parameters for specifying multiple release tag model
+#' 
+#' Parameters for specifying multiple release tag model
+#' 
+#' A list that specifies the following parameters. Mixture of text, integer and
+#' vectors 
+#' 
+#' @seealso \code{\link{multi_release}} for more details.
+#'  
+#' @param mean_wt mean weight of a single fish (optional argument for Chapman 
+#' weight method)
+#' @param method The method to use for calculating population size (options are
+#' "Petersen" or "Chapman")
+#' @param unit the unit of measurement (either "numbers", "kg" or "tonnes")
+#' @param type does fishing occur over a short period (Ricker type 1) or is
+#' extended over the year and competes with natural mortality and tag sheddding
+#' (Ricker type 2) 
+#' @param tag_mort vector of initial tag-induced mortality
+#' @param reporting vector of annual tag reporting rates
+#' @param nat_mort vector of natural mortality (instantaneous)
+#' @param chronic_shed vector of chronic (ongoing) tag shedding
+#' @param chronic_mort vector of chronic (ongoing) tag-induced mortality
+#' @name pars
+NULL
+#> NULL
 
-## I've renamed the 'type' parameter in 'single_release' to 'Ricker', it's clearer
+#' Check multiple release single recapture model inputs
+#' 
+#' Check multiple release single recapture model inputs
+#' 
+#' Check the inputs for \code{\link{multi_release}}
+#' @param tags tag releases and recaptures
+#' @param hauls haul data of the year to calculate the estimate
+#' @param pars model parameters
+#' @name check_mrelease
+NULL       
+#> NULL
 
-## this function now a matrix of tag releases and recaptures, catch and recaptures
+#' @export
+#' @rdname check_mrelease
 check_mrelease_data <- function(tags, hauls){
   ## define the check variable
   check <- TRUE
   ## add checks
-  ## check the releases are > recaptures
-  # if((sum(tags) - sum(recaps)) < 0) 
-  #   stop("more tagged individuals have been recaptured than were released")
-  # ## check the method
-  # ## so we expect to have more than one
-  # if(length(catch) != length(recaps)) stop("catch and recaptures must be of the same length")
-  # ## more checks as they come to mind
-  # ## if catch and recaps are length 1 we can't bootstrap,
-  # ## however this isn't necessarily a problem
-  # ## define the number of years and check vector lengths
-  # n_years <- length(prior_recaps)
   ## return check
   check
 }
 
-#' Check specified parameters
-#' 
-#' Check specified parameters
+#' @export
+#' @rdname check_mrelease
 check_mrelease_pars <- function(pars) {
   ## define the check variable
   check=TRUE
   ## add checks
-  ## check the method
-  # if(!method %in% c("Petersen", "Chapman")) 
-  #   stop("incorrect method, currently 'Petersen' and 'Chapman' are implemented")
-  # ## check units
-  # if(!unit %in% c("numbers", "kg", "tonnes")) 
-  #   stop("incorrect units must be numbers, kg or tonnes")
-  
   ## return check
   check
 }
 
-
+#' Multiple tag release estimate of population size
+#' 
+#' Estimate population size from multiple release, single recapture tagging data
+#' 
 #' For a Type 1 fishery the adjusted releases are calculate as follows
 #' 
 #' 1) Initial tag-induced mortality is applied
@@ -72,17 +87,19 @@ check_mrelease_pars <- function(pars) {
 #' 
 #' 5) The remaining natural mortality and chronic tag loss and tag-induced
 #'  mortality is applied.
-#' @param tags matrix that specifies the vector of the annual numbers of marked animals released (note 
-#' these must be whole numbers)
-#' @param catch - these are by haul!! vector of the number or weight of animals
-#'  captured and checked 
-#' for tags by haul on the second survey
+#' @param tags matrix (or dataframe?) where the first column specifies the annual releases and
+#' subsequent columns the annual recaptures by release cohort (rows). Note 
+#' these must be whole numbers
+#' @param hauls matrix (or dataframe?) where the first column represents the
+#' untagged recaptures and subsequent columns the recaptures by release cohort 
+#' with the first (oldest) cohort to the left and the last (youngest) to the
+#' right
 #' @param pars list of model parameters
 #' @seealso \code{\link{pars}} 
 #' @export
-multi_release <- function(tags, catch, pars)  { # will perhaps add hauls
+multi_release <- function(tags, hauls, pars)  { # will perhaps add hauls
   ## check the inputs
-  # check_data <- check_mrelease_data(tags, catch, recaps, hauls)
+  # check_data <- check_mrelease_data(tags, hauls, recaps, hauls)
   # ## check the parameters
   # check_pars <- check_mrelease_pars(pars)
   ## the checks will identify any problems with the inputs
@@ -164,41 +181,46 @@ multi_release <- function(tags, catch, pars)  { # will perhaps add hauls
     }
   }else stop("either Ricker type 1 or type 2 fishery must be specified")
   ## we now have the available tags by year and would like to estimate population size
-  
-  ## define the assessment year
-  assess_year <- pars[["assess_year"]]
-  ## define storage for the cohort estimates, no need to specify a
+  ## cohort tags
+  n_years <- nrow(hauls)-1
+  ## adjust the recaptures by cohort by reporting rate in the last year
+  recaps_cohort <- colSums(hauls[,-1]) / pars[["reporting"]][n_years]
+  ## the catch (numbers) is the total number of fish 
+  catch <- sum(hauls)
+  ## define storage for the cohort estimates
+  ##* what is recs???
   cohort_est <- rep(NA, nrow(recs))
-
   ## then calculate population size based on the method 
-  ##* these need to be checked and corrected
   if(method=="Petersen"){
-    ## estimate population size for oldest cohort first
-    ## are the recaptures adjusted?
-    est <- petersen(sum(avail_tags), sum(catch), sum(adj_recaps))
+    ## Petersen population estimate overall and by cohort
+    est <- petersen(sum(avail_tags), catch, sum(recap_cohort))
     for(i in 1:nrow(recs)){
-      cohort_est[i] <- petersen(avail_tags[i], QQcatch, recs[i])
+      cohort_est[i] <- petersen(avail_tags[i], catch, recap_cohort[i])
     }
   }else if(method=="Chapman" & unit %in% c("numbers")){
-    ## Chapman numbers
-    est <- chapman_n(avail_tags, sum(catch), adj_recaps)
+    ## Chapman population estimate overall and by cohort
+    est <- chapman_n(sum(avail_tags), catch, sum(recap_cohort))
     for(i in 1:nrow(recs)){
-      cohort_est[i] <- chapman_n(avail_tags[i], QQcatch, recs[i])
+      cohort_est[i] <- chapman_n(avail_tags[i], catch, recap_cohort[i])
     }
   }else if(method=="Chapman" & unit %in% c("kg", "tonnes")){
     ## Chapman weight
-    est <- chapman_wt(avail_tags, sum(catch), adj_recaps, mean_wt)
+    est <- chapman_wt(sum(avail_tags), catch, sum(recap_cohort), pars[["mean_wt"]])
     for(i in 1:nrow(recs)){
-      cohort_est[i] <- chapman_wt(avail_tags[i], QQcatch, recs[i])
+      cohort_est[i] <- chapman_wt(sum(avail_tags), catch, sum(recap_cohort), 
+                                  pars[["mean_wt"]])
     }
   }else stop("method and unit combination not available")
   ## store the data
   ## then collate the results
-  obj <- list("Tags" = tags,
-              "Catch" =catch,
-              "Params" = params,
-              "Estimate" = est,
-              "Residual" = cohort_est - est)
-  ## return the results
+  ##* so this needs to be rewritten
+  obj <- list("Tags" = tags, #* do we need tags?
+              "Hauls" = hauls,
+              "Pars" = pars,
+              "Avail_tags" = avail_tags,
+              "Est" = c(est, cohort_est))
+  ## add class 'mrelease'
+  class(obj) <- 'mrelease'
   obj
 }
+
